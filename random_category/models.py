76d33fe4c908ff6f28c29a90c781dbcd31729a19
EnikoKiraly <tomekrelski@gmail.com>
2021-12-25 18:22:23 +0100

    templates organization and ckeditor add

--- random_category/models.py
+++ random_category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Random_Category(models.Model):
     name = models.CharField(max_length=100)
@@ -12,7 +13,8 @@ class Random_Category(models.Model):
 class Random_Detail(models.Model):
     category = models.ForeignKey(Random_Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000)
+    # description = models.CharField(max_length=1000)
+    description = RichTextField(blank=True, null=True)
     image = models.ImageField(upload_to='assets/img/detail/')
     website = models.CharField(max_length=100)
     location = models.CharField(max_length=1000)
